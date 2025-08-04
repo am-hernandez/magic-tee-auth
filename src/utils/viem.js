a3a86d56f243e5e29a25239b3d47cf7dee5d8f9d
Angel Hernandez <angel@magic.link>
2025-08-04 15:46:21 -0400

    refactor: routes and db

--- src/utils/viem.js
+++
@@ -1,7 +0,0 @@
-import { createPublicClient, http } from 'viem'
-import { baseSepolia } from 'viem/chains'
- 
-export const viemClient = createPublicClient({ 
-  chain: baseSepolia, 
-  transport: http(process.env.ALCHEMY_API_KEY), 
-});
