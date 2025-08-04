c6f01c6eb5ed54003cc6d9832bdc6677a89aa229
Angel Hernandez <angel@magic.link>
2025-08-04 16:47:24 -0400

    feat: add toaster

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { UserProvider } from "@auth0/nextjs-auth0/client";
+import { Toaster } from "react-hot-toast";
 import "./globals.css";
 
 export const metadata = {
@@ -15,6 +16,26 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <UserProvider>{children}</UserProvider>
+        <Toaster
+          position="top-right"
+          toastOptions={{
+            duration: 4000,
+            style: {
+              background: "#363636",
+              color: "#fff",
+            },
+            success: {
+              style: {
+                background: "#059669",
+              },
+            },
+            error: {
+              style: {
+                background: "#dc2626",
+              },
+            },
+          }}
+        />
       </body>
     </html>
   );
